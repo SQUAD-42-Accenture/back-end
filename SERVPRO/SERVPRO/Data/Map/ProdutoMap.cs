edd3e808917ed59a09bc4e818fb04c6c9c0963f1
Rogerio-07 <146589409+Rogerio-07@users.noreply.github.com>
2024-10-30 10:32:06 -0300

    Criação Classe Produto
    Criação das instancias Controller, Models, Maps e Repositorios para Produto

---
+++ SERVPRO/SERVPRO/Data/Map/ProdutoMap.cs
@@ -0,0 +1,14 @@
+﻿namespace SERVPRO.Data.Map
+{
+    public class ProdutoMap
+    {
+        builder.HasKey(x => x.IdProduto).IsRequired().HasMaxLength(6);;
+        builder.Property(x => x.NomeProduto).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.DescricaoProduto).IsnotRequired().HasMaxLength(100);
+        builder.Property(x => x.CategoriaProduto).IsRequired().HasMaxLength(16);
+        builder.Property(x => x.PrecoProduto).IsRequired();
+        builder.Property(x => x.QtdProduto).IsRequired();
+        builder.Property(x => x.DataEntrada).IsRequired();
+
+    }
+}
---
+++ SERVPRO/SERVPRO/Repositorios/interfaces/IProdutoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using SERVPRO.Models;
+
+namespace SERVPRO.Repositorios.interfaces
+{
+    public interface IProdutoRepositorio
+    {
+        Task<List<Produto>> BuscarTodosProdutos();
+        Task<Produto> BuscarPorId(String IdProduto);
+        Task<Produto> BuscarPorNome(String NomeProduto);
+        Task<Produto> Adicionar(Equipamento produto);
+        Task<Produto> Atualizar(Equipamento produto, string IdProduto);
+        Task<bool> Apagar(string IdProduto);
+    }
+}
