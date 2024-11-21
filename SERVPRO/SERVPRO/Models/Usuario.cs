6e660e8391cf7f71153c12e9bbe53946b91ca2f8
Bearodrigues <124224859+Beatriz-Rodriguesx@users.noreply.github.com>
2024-11-21 17:57:24 -0300

    retirada do required Usuario

--- SERVPRO/SERVPRO/Models/Usuario.cs
+++ SERVPRO/SERVPRO/Models/Usuario.cs
@@ -5,16 +5,16 @@ namespace SERVPRO.Models
     public class Usuario
     {
         [JsonPropertyOrder(1)]
-        public required string CPF { get; set; }
+        public string CPF { get; set; }
         [JsonPropertyOrder(2)]
-        public required string Nome { get; set; }
+        public string Nome { get; set; }
         [JsonPropertyOrder(3)]
-        public required string? Email { get; set; }
+        public string? Email { get; set; }
         [JsonPropertyOrder(4)]
-        public required string Senha { get; set; }
-        private required string _tipoUsuario;
+        public string Senha { get; set; }
+        private string _tipoUsuario;
         [JsonPropertyOrder(5)]
-        public required string TipoUsuario
+        public string TipoUsuario
         {
             get => _tipoUsuario;
             set => _tipoUsuario = value;
