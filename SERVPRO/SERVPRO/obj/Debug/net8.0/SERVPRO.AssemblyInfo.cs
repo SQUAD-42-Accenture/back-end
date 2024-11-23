66207eaabee178a4f684b44727f55bd6a78306ae
Bearodrigues <bealimaa.rodrigues@gmail.com>
2024-11-23 03:42:54 -0300

    correção

--- SERVPRO/SERVPRO/obj/Debug/net8.0/SERVPRO.AssemblyInfo.cs
+++ SERVPRO/SERVPRO/obj/Debug/net8.0/SERVPRO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SERVPRO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57ed0c08f12c74da3e089ea8247f3df08906a78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4ad4a738b496032f2631a3c58872db0299d7d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SERVPRO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SERVPRO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
