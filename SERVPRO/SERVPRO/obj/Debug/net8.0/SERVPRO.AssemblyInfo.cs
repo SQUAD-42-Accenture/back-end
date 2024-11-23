22381c54b744138e2c127deee33e9af50861bba4
Vinicius Silva <vinicius.jdsilva98@gmail.com>
2024-11-22 23:21:00 -0300

    blabalu

--- SERVPRO/SERVPRO/obj/Debug/net8.0/SERVPRO.AssemblyInfo.cs
+++ SERVPRO/SERVPRO/obj/Debug/net8.0/SERVPRO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SERVPRO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8658080e5fe197da2e7a94796ab3564214699b51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a686c497d207131a717998666bee43736778f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SERVPRO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SERVPRO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
