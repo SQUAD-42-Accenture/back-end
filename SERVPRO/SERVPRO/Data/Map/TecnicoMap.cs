b464ce5e9a185ca389ef99a43796874ab9da2c48
Vinicius Silva <vinicius.jdsilva98@gmail.com>
2024-11-23 13:45:42 -0300

    ststustecnico

--- SERVPRO/SERVPRO/Data/Map/TecnicoMap.cs
+++ SERVPRO/SERVPRO/Data/Map/TecnicoMap.cs
@@ -13,7 +13,8 @@ namespace SERVPRO.Data.Map
             // builder.Property(x => x.Nome).IsRequired().HasMaxLength(255);
             builder.Property(x => x.Telefone).IsRequired().HasMaxLength(11);
             builder.Property(x => x.Especialidade).IsRequired().HasMaxLength(255);
-           // builder.Property(x => x.Email).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.StatusTecnico).IsRequired().HasMaxLength(255);
+            // builder.Property(x => x.Email).IsRequired().HasMaxLength(255);
             //builder.Property(x => x.Senha).IsRequired().HasMaxLength(255);
             //builder.Property(x => x.TipoUsuario).IsRequired();
 
